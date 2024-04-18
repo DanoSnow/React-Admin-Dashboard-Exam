00a30eb6ef170f32b74fb1a11f8d645947778db8
Daniel <izaguirredaniel9712@gmail.com>
2024-04-18 11:19:41 +0200

    Arreglos en el responsive de la gráfica. Obtención de datos de json-server para visualizar en la gráfica

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -13,26 +13,26 @@ const Dashboard = () => {
             <CardHeader title="Welcome to the administration" />
             <CardContent>
                 <Grid container spacing={2}>
-                    <Grid item md={6}>
+                    <Grid item md={6} sm={12}>
                         <Container>
-                            <Grid container md={12} spacing={2}>
-                                <Grid item md={6}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6} md={6}>
                                     <DashboardCard header="Month sales" content="1538 €" icon={EuroIcon} />
                                 </Grid>
-                                <Grid item md={6}>
+                                <Grid item xs={12} sm={6} md={6}>
                                     <DashboardCard header="New orders" content="18" icon={ShoppingCartIcon} />
                                 </Grid>
-                                <Grid item md={12} sx={{height: '300px', width: '100%'}}>
+                                <Grid item xs={12} sx={{height: '300px', width: 'auto'}}>
                                     <LineChart />
                                 </Grid>
                             </Grid>
                         </Container>
                     </Grid>
-                    <Grid item md={3}>
+                    <Grid item xs={12} md={3} sm={6}>
                         <DashboardCard header="Pending reviews" content="3" icon={MessageIcon}
                         body={['Pending reviews body 1', 'Pending reviews body 2', 'Pending reviews body 3']} />
                     </Grid>
-                    <Grid item md={3}>
+                    <Grid item xs={12} md={3} sm={6}>
                         <DashboardCard header="New customers" content="4" icon={PersonAddRoundedIcon}
                         body={['New customers body 1', 'New customers body 2', 'New customers body 3', 'New customers body 4']} />
                     </Grid>
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,42 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Line } from 'react-chartjs-2'
 import { Chart, CategoryScale, LinearScale, PointElement, Title, Tooltip, Legend, Filler, LineElement } from 'chart.js'
 Chart.register(
     CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler
 );
 
-const sales = [150, 175, 80, 120, 140, 135, 180, 170];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'];
-const data = {
-    labels: months,
-    datasets: [
-        {
-            label: 'Sales',
-            data: sales,
-            tension: 0.5,
-            fill: true,
-            borderColor: 'rgba(255, 99, 132, 0.5)',
-            pointRadius: 5,
-            pointBorderColor: 'rgba(255, 99, 132, 0.5)',
-            pointBackgroundColor: 'rgba(255, 99, 132, 0.5)'
-        }
-    ]
-    
-};
-const options = {
-    maintainAspectRatio: false,
-    scales: {
-        y: {
-            min: 0
-        },
-        x: {
-            ticks: { color: 'rgba(255, 99, 132, 1)' }
-        }
-    }
-};
-
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const LineChart = () => {
+    const [chartData, setChartData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await fetch('http://localhost:3000/sales');
+                if(!response.ok){
+                    throw new Error('Failed to fetch data');
+                }
+                const data = await response.json();
+                setChartData(data.map(e => {
+                    const totalIncome = e.products.reduce((total, product) => total + product.income, 0);
+                    return {
+                        year: e.year,
+                        monthId: e.monthId,
+                        totalIncome: totalIncome
+                    };
+                }));
+            } catch(error){
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+    const sales = chartData.map(e => e.totalIncome);
+    const months = chartData.map(e => MONTHS[e.monthId-1]);
+    
+    const data = {
+        labels: months,
+        datasets: [
+            {
+                label: 'Sales',
+                data: sales,
+                tension: 0.5,
+                fill: true,
+                borderColor: 'rgba(255, 99, 132, 0.5)',
+                pointRadius: 5,
+                pointBorderColor: 'rgba(255, 99, 132, 0.5)',
+                pointBackgroundColor: 'rgba(255, 99, 132, 0.5)'
+            }
+        ]
+    };
+    const options = {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+            y: {
+                min: 0
+            },
+            x: {
+                ticks: { color: 'rgba(255, 99, 132, 1)' }
+            }
+        }
+    };
+
     return <Line data={data} options={options} />;
 }
 
