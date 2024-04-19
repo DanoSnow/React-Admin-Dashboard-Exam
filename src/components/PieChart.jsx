2d9aa36aede3c77ddee3be0bdd2d05b23f4f6acd
Daniel <izaguirredaniel9712@gmail.com>
2024-04-19 13:21:00 +0200

    Segunda mitad de la nueva vista de las ventas individuales

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -6,53 +6,47 @@ Chart.register(
     ArcElement, Tooltip, Legend, 
 );
 
-const PieChart = ({ data }) => {
-    const record = useRecordContext();
-    const [chartData, setChartData] = useState(null);
-    const [chartOptions, setChartOptions] = useState(null);
+const PieChart = ({ chartData }) => {
+    const [data, setData] = useState(null);
     useEffect(() => {
-        const getRecordContext = () => {
-            if(record){
-                const labels = record.products.map(product => product.name);
-                const incomes = record.products.map(product => product.income);
-                const data = {
-                    labels: labels,
-                    datasets: [
-                        {
-                            label: 'Incomes per product',
-                            data: incomes,
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.6)',
-                                'rgba(54, 162, 235, 0.6)',
-                                'rgba(255, 206, 86, 0.6)',
-                                'rgba(75, 192, 192, 0.6)',
-                                'rgba(153, 102, 255, 0.6)',
-                                'rgba(255, 159, 64, 0.6)'
-                            ],
-                            borderColor: [
-                                'rgba(255, 99, 132, 0.6)',
-                                'rgba(54, 162, 235, 0.6)',
-                                'rgba(255, 206, 86, 0.6)',
-                                'rgba(75, 192, 192, 0.6)',
-                                'rgba(153, 102, 255, 0.6)',
-                                'rgba(255, 159, 64, 0.6)'
-                            ],
-                            borderWidth: 1
-                        }
-                    ]
-                };
-                const options = {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                };
-                setChartData(data);
-                setChartOptions(options);
-            }
+        if(chartData){
+            const labels = chartData.map(product => product.name);
+            const incomes = chartData.map(product => product.income);
+            const data_ = {
+                labels: labels,
+                datasets: [
+                    {
+                        label: 'Incomes per product',
+                        data: incomes,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(255, 206, 86, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(153, 102, 255, 0.6)',
+                            'rgba(255, 159, 64, 0.6)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(255, 206, 86, 0.6)',
+                            'rgba(75, 192, 192, 0.6)',
+                            'rgba(153, 102, 255, 0.6)',
+                            'rgba(255, 159, 64, 0.6)'
+                        ],
+                        borderWidth: 1
+                    }
+                ]
+            };
+            setData(data_);
         }
-        getRecordContext();
-    }, [record]);
-    
-    return chartData && chartOptions ? <Pie data={chartData} options={chartOptions} /> : null;
+    }, [chartData]);
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+    };
+
+    return data ? <Pie data={data} options={options} /> : null;
 }
 
 export default PieChart
--- src/components/ShowWrapper.jsx
+++ src/components/ShowWrapper.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Box, Typography } from "@mui/material"
+import { Box, Typography, Card } from "@mui/material"
 import PieChart from './PieChart'
 import { useRecordContext } from 'react-admin'
 
@@ -12,17 +12,23 @@ const ShowWrapper = () => {
     useEffect(() => {
         if(record){
             setTotalIncome(record.products.reduce((total, product) => total + product.income, 0));
+            setChartData(record.products.map(e => {
+                return {
+                    name: e.name,
+                    income: e.income
+                }
+            }));
         }
     }, [record]);
 
     return (
-        <Container>
-            <Typography display='flex' justifyContent='center' my={2} variant='h4'>Statistics of {record ? `${MONTHS[record.monthId-1]} ${record.year}` : null}</Typography>
+        <Card sx={{width: '100%', padding: '24px 0'}}>
+            <Typography display='flex' justifyContent='center' variant='h4'>Statistics of {record ? `${MONTHS[record.monthId-1]} ${record.year}` : null}</Typography>
             <Typography display='flex' justifyContent='center' variant='h6'>Total income: {totalIncome ? totalIncome : null} €</Typography>
             <Box sx={{height: '300px'}}>
-                <PieChart />
+                <PieChart chartData={chartData}/>
             </Box>
-        </Container>
+        </Card>
     )
 }
 
